cbcfa677ebcee13f96d30755e69e211db4dda61d
rahem sorour <sorourrahem17@gmail.com>
2023-08-10 05:51:26 +0100

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'someone';
+
+// Write a function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+   return  customerName = customerName.toUpperCase();  
+}
+
+
+// Write a function to set the bestCustomer variable
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+
+// Write a function to overwrite the bestCustomer variable
+function overwriteBestCustomer() {
+  return bestCustomer = 'maybe bob';
+}
+
+
+// Write a function to attempt changing the constant leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer ='newval';
+}
+
